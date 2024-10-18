d585869cc790503e13ba4f49de4de20625736745
TothMartonZsombor <77171552+TothMartonZsombor@users.noreply.github.com>
2024-10-18 10:30:43 +0200

    mantes

--- elearningLara/app/Models/Answer.php
+++ elearningLara/app/Models/Answer.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Answer extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['answer_text', 'question_id', 'is_discorrect'];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
--- elearningLara/app/Models/Scores.php
+++ elearningLara/app/Models/Scores.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Scores extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['subject_id', 'score', 'email'];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
 }
